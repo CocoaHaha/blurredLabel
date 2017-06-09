8c29d25ac4dafa9bcb4dbd49e089cf42496c0057
CocoaHaha <1076800596@qq.com>
2017-06-09 15:25:34 +0800

    Initial Commit

---
+++ RuntimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeDemo
+//
+//  Created by 石向锋 on 2017/6/9.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RuntimeDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RuntimeDemo
+//
+//  Created by 石向锋 on 2017/6/9.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
